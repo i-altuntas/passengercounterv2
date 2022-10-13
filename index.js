273a11fb6e6e2632f79ab7c261f02244db41234a
faruk mete <i.altuntas011@gmail.com>
2022-10-13 23:50:39 +0300

    totalPeople özelliği eklendi

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let saveEl = document.getElementById("save-el")
 let gecmisBilgi = localStorage.getItem("gecmisBilgi")
+
+
 if( gecmisBilgi !== null) {
     saveEl.textContent = localStorage.getItem("gecmisBilgi")
 } else {
@@ -7,16 +9,41 @@ if( gecmisBilgi !== null) {
 }
 
 
+let gecmisBilgi2 = localStorage.getItem("gecmisBilgi2")
+let totalPeople = document.querySelector("#total-people");
+if(gecmisBilgi2 !== null){
+    totalPeople.textContent = localStorage.getItem("gecmisBilgi2")
+}else {
+    totalPeople.textContent = ""
+}
+
+
+///////////////////////////////////////////////////////7
 let countEl = document.getElementById("count-el")
 let count = 0
-
 function increment() {
     count += 1
     countEl.textContent = count
 }
+/////////////////////////////////////////////////////////
+let totalPeople_mevcut = 0;
+if(gecmisBilgi2 === null){
+    totalPeople_mevcut =0;
 
+}else{
+    totalPeople_mevcut=Number(gecmisBilgi2);
+}
+
+function total (){
+    
+    totalPeople_mevcut =totalPeople_mevcut + count;
+    totalPeople.textContent="Total people: " + totalPeople_mevcut;
+    localStorage.setItem( "gecmisBilgi2", totalPeople_mevcut)
+}
+///////////////////////////////////////////////////////////
 function save() {
-    let countStr = count + " - "
+    total ();
+    let countStr = count + " - ";
     saveEl.textContent += countStr
     localStorage.setItem( "gecmisBilgi", saveEl.textContent )
     countEl.textContent = 0
